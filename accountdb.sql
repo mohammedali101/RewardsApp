96eb0f0d91a491602a035f7cd74d5ac9f0cb84e0
ausn1 <hd6839@wayne.edu>
2025-02-11 18:26:35 -0500

    Initial testing of signup page

---
+++ accountdb.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS db;
+
+CREATE DATABASE db;
+
+USE db;
+
+CREATE TABLE accounts(
+    ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username varchar(30) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    balance int DEFAULT 10
+);
+
+CREATE TABLE admins(
+    ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username varchar(30) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+    company TEXT NOT NULL,
+);
